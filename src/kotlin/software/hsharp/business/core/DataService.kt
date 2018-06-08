b654b27ac2f01407b6a506d907d175b750f7c496
davidpodhola <david@podhola.net>
2018-06-08 15:11:01 +0200

    GetRowResultObj to be able to serialize an object (e.g. returned from iDempiere)

--- src/kotlin/software/hsharp/business/core/DataService.kt
+++ src/kotlin/software/hsharp/business/core/DataService.kt
@@ -1,7 +1,17 @@
 package software.hsharp.business.core
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.compiere.orm.DefaultModelFactory
+import org.compiere.orm.IModelFactory
+import org.compiere.orm.MTable
+import org.compiere.process.ProcessInfo
+import org.compiere.process.ProcessInfoParameter
+import org.compiere.process.ProcessUtil
 import org.idempiere.common.util.DB
 import org.idempiere.common.util.Env
+import org.idempiere.common.util.Env.getAD_Client_ID
+import org.idempiere.common.util.Trx
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import software.hsharp.core.models.*
@@ -22,7 +32,12 @@ data class GetDataResult(
 data class GetRowResult(
         override val rs: ResultSet?,
         override val __metadata: IDataSource?,
-        override val __paging: IPaging?) : IGetRowResult
+        override val __paging: IPaging?) : IGetRowResultData
+
+data class GetRowResultObj(
+        override val o: Any?,
+        override val __metadata: IDataSource?,
+        override val __paging: IPaging?) : IGetRowResultObject
 
 data class UpdateDataResult(
         override val id: Int?,
@@ -44,6 +59,11 @@ data class CreateDataResult(
     }
 }
 
+data class ExecuteJavaProcessResult(
+        val message : String?,
+        val success : Boolean
+)
+
 @Component
 class DataService : IDataService {
     override fun getTreeData(connection: Connection, root: ITreeDataDescriptor, orderBy: String, orderByOrder: String, offset: Int, limit: Int, filterName1: String, filterValue1: String, filterName2: String, filterValue2: String): IGetTreeDataResult {
@@ -51,7 +71,29 @@ class DataService : IDataService {
     }
 
     override fun execute(connection: Connection, procName: String, jsonBody: String): String? {
-        TODO("Implement execute for iDempiere too")
+        val ctx = Env.getCtx()
+        val ad_Client_ID = Env.getAD_Client_ID(ctx)
+        val ad_Org_ID = Env.getAD_Org_ID(ctx)
+        val ad_User_ID = Env.getAD_User_ID(ctx)
+        val cnn = DB.getConnectionRW()
+
+        set_user(cnn, ad_User_ID)
+
+        val parameters : Array<ProcessInfoParameter> = arrayOf(
+                ProcessInfoParameter( "AD_Client_ID", ad_Client_ID.toBigDecimal(), null, null, null ),
+                ProcessInfoParameter( "AD_Org_ID", ad_Org_ID.toBigDecimal(), null, null, null )
+        )
+
+        val processInfo = ProcessInfo("Execute Java Process", 0)
+        processInfo.aD_Client_ID = ad_Client_ID
+        processInfo.aD_User_ID = ad_User_ID
+        processInfo.parameter = parameters
+        processInfo.className = procName
+        val m_trx = Trx.get(Trx.createTrxName("ExecuteJavaProcess"), true)
+        val success = ProcessUtil.startJavaProcess(ctx, processInfo, m_trx, false)
+        val result = ExecuteJavaProcessResult( processInfo.summary, success )
+        val mapper = ObjectMapper().registerModule(KotlinModule())
+        return mapper.writeValueAsString(result)
     }
 
     override fun getSchemasSupported(connection:Connection) : Array<String> {
@@ -75,8 +117,8 @@ class DataService : IDataService {
         set_user(cnn, ad_User_ID)
 
         val sql =
-                ( fields.fold( "INSERT INTO \"${tableName}\" (", { total, next -> total + next.first + "," } ) ).trimEnd(',') +
-                        ( fields.fold( " ) VALUES (", { total, _ -> "$total?," } ) ).trimEnd(',') + ") RETURNING ${tableName}_id;";
+                ( fields.fold( "INSERT INTO \"${tableName}\" ( \"${tableName}_id\",", { total, next -> total + next.first + "," } ) ) +
+                        ( fields.fold( "ad_client_id, ad_org_id ) VALUES ( (SELECT COALESCE(MAX(\"${tableName}_id\"),0) + 1 FROM \"${tableName}\" ),", { total, _ -> "$total?," } ) ) + " ?, ?) RETURNING ${tableName}_id;";
 
         System.out.println( "createData SQL:$sql" );
 
@@ -89,6 +131,9 @@ class DataService : IDataService {
                 throw ex
             }
         }
+        val fieldsCount = fields.count()
+        statement.setInt(fieldsCount + 1, ad_Client_ID)
+        statement.setInt(fieldsCount + 2, ad_Org_ID)
 
         val rs = statement.executeQuery()
         connection.commit()
@@ -118,14 +163,18 @@ class DataService : IDataService {
         set_user(cnn, ad_User_ID)
 
         val sql =
-                ( fields.fold( "UPDATE \"${tableName}\" SET ", { total, next -> total + "${next.first}=?," } ) ).trimEnd(',') +
-                        " WHERE ${tableName}_id = ? RETURNING ${tableName}_id;";
+                ( fields.fold( "UPDATE \"${tableName}\" SET ", { total, next -> total + "${next.first}=?," } ) ) +
+                        "ad_client_id = ?, ad_org_id = ? WHERE ${tableName}_id = ? RETURNING ${tableName}_id;";
 
         System.out.println( "SQL:$sql, ID:$id" );
 
         val statement = cnn.prepareStatement(sql)
         fields.forEachIndexed { index, value -> statement.setObject( index + 1, value.second ) }
-        statement.setInt(fields.count()+1, id)
+
+        val fieldsCount = fields.count()
+        statement.setInt(fieldsCount + 1, ad_Client_ID)
+        statement.setInt(fieldsCount + 2, ad_Org_ID)
+        statement.setInt(fieldsCount + 3, id)
 
         val rs = statement.executeQuery()
         // cnn.commit() <- auto commit
@@ -161,8 +210,9 @@ class DataService : IDataService {
 
         set_user(cnn, ad_User_ID)
 
+        val tableNameLowerCase = tableName.toLowerCase()
         val selectPart = "SELECT *"
-        val sql =  "$selectPart, set_user(?) FROM \"${tableName}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) AND \"${tableName}_id\" = ? "
+        val sql =  "$selectPart, set_user(?) FROM \"${tableNameLowerCase}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) AND \"${tableNameLowerCase}_id\" = ? "
         println ( "Row SQL:$sql" )
         val statement = cnn.prepareStatement(sql)
         statement.setInt(1, ad_User_ID)
@@ -171,8 +221,15 @@ class DataService : IDataService {
         statement.setInt(4, id)
         val rs = statement.executeQuery()
 
-        return GetRowResult( rs = rs, __metadata = null, __paging = null )
-
+        val table = MTable.get( ctx, tableName )
+        if ( table == null ) {
+            return GetRowResult( rs = rs, __metadata = null, __paging = null )
+        } else {
+            rs.next()
+            val modelFactory : IModelFactory = DefaultModelFactory()
+            val result = modelFactory.getPO( tableName, rs, "getRow")
+            return GetRowResultObj( o = result, __metadata = null, __paging = null )
+        }
     }
 
 
@@ -232,7 +289,12 @@ class DataService : IDataService {
           else
             { columnsRequested.fold( "SELECT ", { total, next -> "$total \"$next\"," } ).trimEnd(',') }
 
-        val sql =  "$selectPart, set_user(?)  FROM \"${tableName}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) $where_clause LIMIT $limit OFFSET $offset;"
+        val sql =
+                "$selectPart, set_user(?)  FROM \"${tableName}\" WHERE (ad_client_id = ? OR ad_client_id=0) AND (ad_org_id = ? OR ad_org_id=0) $where_clause" +
+                if ( orderBy != "" ) {
+                    " ORDER BY \"$orderBy\"" + if ( orderByOrder.toLowerCase() == "desc" ) { " desc" } else { "" }
+                } else { " ORDER BY 1" } +
+                " LIMIT $limit OFFSET $offset;"
         println ( "SQL:$sql" )
         val statement = cnn.prepareStatement(sql)
         statement.setInt(1, ad_User_ID)
